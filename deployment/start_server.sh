f711483d8228aa7a0574ed55e5c942830268ffd9
Eli White <elibbw@gmail.com>
2016-04-19 04:56:49 -0400

    update build script

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /usr/local/maven/current/bin/mvn package -f /var/trtl/pom.xml -DskipTests=true
-sudo nohup /var/trtl/run --port 80 --gui &
+sudo nohup /var/trtl/deployment/server_run.sh --port 80 --gui &
 PID=$!
 echo $PID > /var/trtl/pid.txt
