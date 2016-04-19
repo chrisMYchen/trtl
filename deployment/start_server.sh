09ccdfe4a38af81e3317498d97dad1bcf4104276
Eli White <elibbw@gmail.com>
2016-04-19 19:05:07 -0400

    update scripts

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+PID_FILE=/var/trtl/trtl-pid
 sudo rm -f /var/trtl/.mvn-classpath
 sudo /usr/local/maven/current/bin/mvn package -f /var/trtl/pom.xml -DskipTests=true
-sudo nohup sudo /var/trtl/deployment/server_run.sh --port 80 --gui &
+if [ ! -f $PID_FILE ]; then
+  sudo nohup sudo /var/trtl/deployment/server_run.sh --port 80 --gui 2>> /dev/null >> /dev/null &
+    echo $! > $PID_FILE
+fi
--- deployment/stop_server.sh
+++ deployment/stop_server.sh
@@ -1,1 +1,6 @@
-sudo pkill java
+PID_FILE=/var/trtl/trtl-pid
+if [ -f $PID_FILE ]; then
+  PID=$(cat $PID_FILE);
+  sudo kill $PID
+  rm -f $PID_FILE
+fi
