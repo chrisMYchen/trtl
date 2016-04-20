1446e9b018bfccd2530abab47d54967955ce7996
Eli White <elibbw@gmail.com>
2016-04-20 03:16:16 -0400

    Front-end code restructuring, backend communication

---
+++ src/main/resources/static/js/location.js
@@ -0,0 +1,28 @@
+var loc_options = {
+  enableHighAccuracy: true,
+  timeout: 27000,
+  maximumAge: 30000
+}
+
+function locationStart(){
+  if(navigator.geolocation){
+    var id = navigator.geolocation.watchPosition(
+      function(data){
+        locationHandler({lat: data.coords.latitude, lng: data.coords.longitude});
+      },
+      function(error) {
+        locationError(error, id);
+      }, loc_options);
+  } else{
+    $("#location").html("Location not available");
+  }
+}
+
+function locationError(error, id){
+    navigator.geolocation.clearWatch(id);
+    $("#location").html(error.message);
+}
+
+function locationHandler(position){
+  $("#location").html("Your location is: " + position.lat + ", " + position.lng);
+}
