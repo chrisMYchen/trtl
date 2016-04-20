f03eada9f80b2fc24cf0a9cf17779f776a2f6133
chrisMYchen <christopher_chen@brown.edu>
2016-04-19 20:51:57 -0400

    basic functionality and spark servers set up

--- src/main/java/edu/brown/cs/ebwhite/scavenger/SparkServer.java
+++ src/main/java/edu/brown/cs/ebwhite/scavenger/SparkServer.java
@@ -1,6 +1,13 @@
 package edu.brown.cs.ebwhite.scavenger;
 
-import freemarker.template.Configuration;
+import geo.LatLong;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import note.Note;
 import spark.ModelAndView;
 import spark.QueryParamsMap;
 import spark.Request;
@@ -10,45 +17,143 @@ import spark.Spark;
 import spark.TemplateViewRoute;
 import spark.template.freemarker.FreeMarkerEngine;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Set;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 
+import database.TurtleQuery;
 
-public class SparkServer{
+public class SparkServer {
   Gson GSON;
 
-  public SparkServer(int port){
+  public SparkServer(int port) {
     GSON = new Gson();
     Spark.setPort(port);
     Spark.externalStaticFileLocation("src/main/resources/static");
+
   }
 
-  public void run(){
+  public void run() {
     Spark.get("/home", new HomeHandler(), new FreeMarkerEngine());
+    Spark.post("/getNotes", new GetNotes());
+    Spark.post("/updateNotes", new UpdateNotes());
+    Spark.post("/postNote", new PostNote());
   }
 
   private class HomeHandler implements TemplateViewRoute {
     @Override
     public ModelAndView handle(Request req, Response res) {
-      Map<String, Object> variables = ImmutableMap.of(
-      "title", "trtl"
-      );
+      Map<String, Object> variables = ImmutableMap
+          .of("title", "trtl");
       return new ModelAndView(variables, "home.ftl");
     }
   }
+
+  private class GetNotes implements Route {
+    @Override
+    public Object handle(final Request req, final Response res) {
+      QueryParamsMap qm = req.queryMap();
+      String uIDstring = qm.value("userID");
+      String latString = qm.value("lat");
+      String lonString = qm.value("lon");
+      String timeString = qm.value("timestamp");
+      String minPostString = qm.value("minPost");
+      String maxPostString = qm.value("maxPost");
+      String radiusString = qm.value("radius");
+      String message = "";
+      List<Note> notes = new ArrayList<>();
+      try {
+        int uID = Integer.parseInt(uIDstring);
+        double lat = Double.parseDouble(latString);
+        double lon = Double.parseDouble(lonString);
+        int timestamp = Integer.parseInt(timeString);
+        int minPost = Integer.parseInt(minPostString);
+        int maxPost = Integer.parseInt(maxPostString);
+        double radius = Double.parseDouble(radiusString);
+        notes = TurtleQuery.getNotes(new LatLong(lat, lon), radius, minPost,
+            maxPost, timestamp);
+
+      } catch (NullPointerException np) {
+        message = "Fields not filled. smtn null.";
+      } catch (NumberFormatException nfe) {
+        message = "number format exception.";
+      } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        message = "SQL error.";
+      }
+      Map<String, Object> variables = new ImmutableMap.Builder().put(
+          "notes", notes).put("error", message).build();
+      return GSON.toJson(variables);
+    }
+  }
+
+  private class UpdateNotes implements Route {
+    @Override
+    public Object handle(final Request req, final Response res) {
+      QueryParamsMap qm = req.queryMap();
+      String uIDstring = qm.value("userID");
+      String latString = qm.value("lat");
+      String lonString = qm.value("lon");
+      String timeString = qm.value("timestamp");
+      String minPostString = qm.value("minPost");
+      String maxPostString = qm.value("maxPost");
+      String radiusString = qm.value("radius");
+      String message = "";
+      List<Note> notes = new ArrayList<>();
+      try {
+        int uID = Integer.parseInt(uIDstring);
+        double lat = Double.parseDouble(latString);
+        double lon = Double.parseDouble(lonString);
+        int timestamp = Integer.parseInt(timeString);
+        int minPost = Integer.parseInt(minPostString);
+        int maxPost = Integer.parseInt(maxPostString);
+        double radius = Double.parseDouble(radiusString);
+        notes = TurtleQuery.updateNotes(new LatLong(lat, lon), radius, minPost,
+            maxPost, timestamp);
+
+      } catch (NullPointerException np) {
+        message = "Fields not filled. smtn null.";
+      } catch (NumberFormatException nfe) {
+        message = "number format exception.";
+      } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        message = "SQL error.";
+      }
+      Map<String, Object> variables = new ImmutableMap.Builder().put(
+          "notes", notes).put("error", message).build();
+      return GSON.toJson(variables);
+    }
+  }
+
+  private class PostNote implements Route {
+    @Override
+    public Object handle(final Request req, final Response res) {
+      QueryParamsMap qm = req.queryMap();
+      String uIDstring = qm.value("userID");
+      String latString = qm.value("lat");
+      String lonString = qm.value("lon");
+      String timeString = qm.value("timestamp");
+      String content = qm.value("text");
+      String message = "";
+
+      try {
+        int uID = Integer.parseInt(uIDstring);
+        double lat = Double.parseDouble(latString);
+        double lon = Double.parseDouble(lonString);
+        int timestamp = Integer.parseInt(timeString);
+
+        TurtleQuery.postNote(uID, timestamp, lat, lon, content);
+
+      } catch (NullPointerException np) {
+        message = "Fields not filled. smtn null.";
+      } catch (NumberFormatException nfe) {
+        message = "number format exception.";
+      } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        message = "SQL error when posting note.";
+      }
+      Map<String, Object> variables = new ImmutableMap.Builder().put("error", message).build();
+      return GSON.toJson(variables);
+    }
+  }
+
 }
