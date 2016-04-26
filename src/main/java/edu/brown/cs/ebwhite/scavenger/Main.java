6199f5593a2b11117d055c995a5f7585a0b12281
Eli White <elibbw@gmail.com>
2016-04-26 12:32:20 -0400

    Change DB location

--- src/main/java/edu/brown/cs/ebwhite/scavenger/Main.java
+++ src/main/java/edu/brown/cs/ebwhite/scavenger/Main.java
@@ -14,79 +14,15 @@ import edu.brown.cs.ebwhite.note.Note;
 import edu.brown.cs.ebwhite.note.NoteRanker;
 
 public class Main {
-  private Integer port;
 
   public static void main(String[] args) {
-    new Main(args).run();
-  }
-
-  private String[] args;
-
-  private Main(String[] args) {
-    this.args = args;
-
-    try {
-      Db.database("turtlDB.sqlite3");
-    } catch (ClassNotFoundException e1) {
-      System.out.println("ERROR: could not connect to DB");
-      e1.printStackTrace();
-    }
-
-    try {
-      // adding a user
-
-      // int uID = TurtleQuery.addUser("newUser", "password", "meep", null,
-      // "asdffdsa@gmail.com", -1);
-      // System.out.println(uID);
-      //
-      // TurtleQuery.postNote(3, 1010100, 71, -41, "hemang3. early note.",
-      // 1);
-      // TurtleQuery.postNote(4, 1010100, 71, -41, "new user. early note.",
-      // 1);
-
-      NoteRanker noteRank = new NoteRanker();
-      noteRank.setCurrentUser(3);
-      // TurtleQuery.postNote(1, 10101010, 71, -41,
-      // "This is the second note we ever posted", 0);
-      // TurtleQuery.postNote(1, 10101010, 71, -41,
-      // "This is the first private note by katie we ever posted", 1);
-      // TurtleQuery.postNote(2, 10101010, 71, -41,
-      // "This is the second private note by chris we ever posted", 1);
-      // Testing posting notes and time sorting
-      // TurtleQuery.postNote(2, 101010, 71, -41,
-      // "This is the third note.", 0);
-      List<Note> notes = new ArrayList<>();
-      notes = TurtleQuery.getNotes(3, new LatLong(71.0005, -41.0005),
-          50, 0, 100, 10101011);
-      List<Note> notes2 = TurtleQuery.getNotes(-1, new LatLong(71.0001,
-          -41.0001),
- 500, 0, 100, 10101011);
-      notes.sort(noteRank);
-      for (Note n : notes) {
-        System.out.println(String.format("note id %d is, %s", n.getId(),
-            n.getText()));
-      }
-      // notes = TurtleQuery.getNotesAnonymous(new LatLong(71, -41), 500,
-      // 4, 12,
-      // 10101010);
-      // if (notes.isEmpty()) {
-      // System.out.println("No notes from this timestamp.");
-      // }
-
-    } catch (SQLException e) {
-      System.out.println("Error posting note.");
-      e.printStackTrace();
-    }
-  }
-
-  private void run() {
+    Integer port;
     OptionParser parser = new OptionParser();
     parser.accepts("gui");
     parser.accepts("port").withRequiredArg().ofType(Integer.class);
+    parser.accepts("database").withRequiredArg().ofType(String.class);
     OptionSpec<String> stringSpec = parser.nonOptions().ofType(String.class);
     OptionSet options = parser.parse(args);
-
-    /* List<String> input = options.valuesOf(stringSpec); */
 
     if (options.has("port")) {
       port = (Integer) options.valueOf("port");
@@ -94,12 +30,25 @@ public class Main {
       port = 2456;
     }
 
+    if(options.has("database")){
+      String path = (String) options.valueOf("database");
+      try {
+        Db.database(path);
+      } catch (ClassNotFoundException e1) {
+        System.out.println("ERROR: could not connect to DB: " + path);
+        e1.printStackTrace();
+      }
+    }
+    else{
+      try {
+        Db.database("turtlDB.sqlite3");
+      } catch (ClassNotFoundException e1) {
+        System.out.println("ERROR: could not connect to DB turtlDB.sqlite3");
+        e1.printStackTrace();
+      }
+    }
+
     if (options.has("gui")) {
-      /*
-       * if(input.size() != 1){
-       * System.out.println("ERROR: Wrong number of arguments");
-       * System.exit(1); }
-       */
       /* Start spark, etc. */
       SparkServer s = new SparkServer(port);
       s.run();
@@ -107,5 +56,4 @@ public class Main {
       return;
     }
   }
-
 }
