966338e31956824614613b79de717ebdc9db8661
Eli White <elibbw@gmail.com>
2016-04-28 18:54:02 -0400

    SSL updates

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -2,4 +2,3 @@
 sudo rm -f /var/trtl/.mvn-classpath
 sudo /usr/local/maven/current/bin/mvn package -f /var/trtl/pom.xml -DskipTests=true >> /var/trtl/trtl.log
 /var/trtl/deployment/server_run.sh --gui --port 443 --keystore /var/SSL/server.keystore --keypass ME1trtlapp --database /home/ec2-user/turtlDB.sqlite3 2>> /var/trtl/trtl.log >> /var/trtl/trtl.log
-/var/trtl/deployment/server_run.sh --gui --port 80 --database /home/ec2-user/turtlDB.sqlite3 2>> /var/trtl/trtls.log >> /var/trtl/trtls.log
