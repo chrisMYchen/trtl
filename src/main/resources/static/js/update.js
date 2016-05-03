e73fef32376ecd935cf15c3b48fc5cd3d7dd8e2c
Eli White <elibbw@gmail.com>
2016-05-03 12:31:15 -0400

    On live update, scroll to maintain position on page.

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -6,14 +6,21 @@ function update(time, radius){
 
 function updatesDOM(notes, start){
 
-   for(var i = 0; i < notes.length; i++){
-       var note = notes[i];
-       note.order = start + i;
-       var dom = $("<div></div>").attr("class","post").attr("data-order", note.order);
-       $("#updates").prepend(dom);
-       note.dom = dom;
-       processNote(note);
-   }
+  for(var i = 0; i < notes.length; i++){
+    var height_before = $("#updates").height();
+    var position_before = $(window).scrollTop();
+    var note = notes[i];
+    note.order = start + i;
+    var dom = $("<div></div>").attr("class","post").attr("data-order", note.order);
+    $("#updates").prepend(dom);
+    note.dom = dom;
+    processNote(note);
+    if(position_before > 20){
+      var height_after = $("#updates").height();
+      var difference = height_after - height_before;
+      $(window).scrollTop(position_before + difference);
+    }
+  }
 }
 
 function updateNotes(time, radius){
