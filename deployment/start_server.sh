f452ef2919c0b7c1e55dad1c003f15d6acc54185
Christopher Chen <christopher_chen@brown.edu>
2016-10-25 00:04:05 -0400

    Delete start_server.sh

--- deployment/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo rm -f /var/trtl/.mvn-classpath
-sudo /usr/local/maven/current/bin/mvn package -f /var/trtl/pom.xml -DskipTests=true >> /var/trtl/trtl.log
-/var/trtl/deployment/server_run.sh --gui --port 443 --keystore /var/SSL/trtl.keystore --keypass ME1trtlapp --database /home/ec2-user/turtlDB.sqlite3 2>> /var/trtl/trtl.log >> /var/trtl/trtl.log
